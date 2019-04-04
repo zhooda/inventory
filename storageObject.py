c33a4c3b58a789d9df23b2cf5b449453b92d6c64
Zeeshan <zeeshanhooda@icloud.com>
2019-04-04 13:56:42 -0600

    storage object class basic

--- storageObject.py
+++ storageObject.py
@@ -10,4 +10,19 @@ class binClass:
 	def __init__(self, name, slotsTotal, slotsFree):
 		self.name = name
 		self.slotsTotal = slotsTotal
-		self.slotsFree = slotsFr		
+		self.slotsUsed = slotsFree
+		self.slotsUsed = 0
+		self.storedObj = []
+		
+	def getName(self):
+		return self.name
+	
+	def getTotalSlots(self):
+		return self.slotsTotal
+		
+	def getUsedSlots(self):
+		self.slotsFree = getTotalSlots - 
+		return len(self.storedObj)
+		
+	def getFreeSlots(self):
+				
